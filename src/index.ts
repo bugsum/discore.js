5388145e89e44ea2dffacab272a0faec0de1c562
Samarth Sharma <devinsane.79@gmail.com>
2024-11-27 12:55:59 +0530

    Added Express

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import { GatewayIntentBits as Intents } from 'discord.js';
 import { Discore } from './discore';
 import { getEnv } from './utils/env';
+import express from 'express';
 
 const instance = new Discore({
     intents: [Intents.Guilds, Intents.GuildMembers, Intents.GuildMessages],
 });
 
 instance.initialize(getEnv('CLIENT_TOKEN'));
+
+const app = express();
+
+app.get('/', (_, res) => {
+    res.send('Discord Bot is online');
+});
+
+app.listen(3000, () => console.log('Express server running on port 3000'));
