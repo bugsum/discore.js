e93d2eaa1be577358645a467129ad5806ebafc8a
Samarth Sharma <devinsane.79@gmail.com>
2024-11-30 00:00:25 +0530

    Removed showping test command.

--- src/events/message.ts
+++ src/events/message.ts
@@ -2,8 +2,4 @@ import { createEvent, logger } from '../utils';
 
 export default createEvent('messageCreate', false, (instance, message) => {
     if (message.author.id === instance.user?.id) return;
-
-    if (message.content === 'showping') {
-        message.reply(`My ping is ${instance.ws.ping}ms`);
-    }
 });
